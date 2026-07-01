fd652ee7c9173dabcdc38004505955ff7b540304
Maria Crist <cristeduarda1@gmail.com>
2026-06-30 21:14:33 -0300

    Update STension2DompV5.f90
    documenting code

--- STension2DompV5.f90
+++ STension2DompV5.f90
@@ -1,176 +1,289 @@
-PROGRAM stension_final
-implicit none
+! ===========================================================================
+! STension2DompV5.f90 — interfacial/surface tension from per-atom stress
+! ===========================================================================
+!
+! computes the interfacial (surface) tension γ from the per-atom pressure
+! tensor using the mechanical (Irving-Kirkwood) definition:
+!
+!     γ = (1/2) × ∫ [Pzz(z) - (Pxx(z) + Pyy(z))/2] dz
+!
+! the factor 1/2 accounts for two interfaces (top and bottom).
+! also outputs the time-averaged local pressure anisotropy profile P(z).
+!
+! units:
+!   lammps stress/atom outputs in pressure × volume [bar·å³] (real/metal units).
+!   dividing by bin volume gives [bar].
+!   integration over z gives [bar·å].
+!   conversion: bar·å × 1e5 (pa/bar) × 1e-10 (m/å) × 1e3 (mn/m per n/m) = × 1e-2
+!   final unit: mn/m  (= dyn/cm in cgs, the common experimental unit for γ).
+!
+! sign convention:
+!   lammps stress/atom stores the negative of the pressure tensor (compressive
+!   positive). we apply a global sign flip when binning so that the result
+!   represents the standard mechanical pressure (tensile positive).
+!
+! dump format expected:
+!   compute   stress all stress/atom NULL virial
+!   dump      d2 all custom 5000 traj_stress.lammpstrj &
+!             id type mol xu yu zu c_stress[1] c_stress[2] c_stress[3]
+!   dump_modify d2 sort id
+!   columns: id  type  mol  xu  yu  zu  pxx  pyy  pzz
+!
+!
+! THINGS YOU MUST EDIT! (marked user edit):
+!   nconf, nbins, jump, inputtraj, outputfile
+!
+! output:
+!   screen (each frame): frame  γ_this_frame(mn/m)  γ_running_avg(mn/m)
+!   screen (final):      γ_avg  std_dev  std_err   (all in mn/m)
+!   file pressures.dat:  z(å)  p(z)(bar)           (time-averaged profile)
+! ===========================================================================
+program stension_final
+  implicit none
 
-character(50)            ::inputfile
-integer                  ::nconf,j,l,k,d,o,i,tid
-integer                  ::natoms
-integer                  ::nbins,m
-real*8,parameter         ::pi=3.14159
-integer                  ::ntype,b,dummyint,nnumber,jump
-character(100)           ::dummychar,id
-real*8                   ::t1,delta,vx,vy,vz
-real*8                   ::hix,lox,temp,sigmabig,temp2,bla
-real*8                   ::hiy,loy,hiz,loz,std,ste
-real*8                   ::x,y,z,rxij,ryij,rzij
-real*8,dimension(:),allocatable :: Pxx,Pyy,Pzz,Pxy,Pxz,Pyz,temp1
-real*8                   ::dp,rmax,dr,low,high,r,dV,norm,theta
-real*8                   ::xh,xl,yh,yl,zh,zl
-real*8                   ::dummy
-real*8,dimension(:),allocatable          ::xx,yy,zz,Ptot,pwater,dpdr
-real*8,dimension(:),allocatable          ::Ptot1,Ptot2,Ptot3,Ptot4,psili,sigma
-real*8,dimension(:,:),allocatable        ::soma
-integer,dimension(:),allocatable       ::nntype
-integer,dimension(:),allocatable  ::idd
-!ccccccccccccccccccccccccccccccccccccccccc
+  ! ---------------------------------------------------------------------------
+  ! user edit — analysis parameters
+  ! ---------------------------------------------------------------------------
+  integer,            parameter :: nconf      = 800    ! production frames to process
+  integer,            parameter :: nbins      = 210    ! z-slabs along interface normal
+  integer,            parameter :: jump       = 0      ! equilibration frames to skip; 0 if not present
+  character(len=100), parameter :: inputtraj  = 'traj_stress.lammpstrj'
+  character(len=100), parameter :: outputfile = 'pressures.dat'
+  ! ---------------------------------------------------------------------------
 
-!Setting de walltime timer
-t1=secnds(0.0)
+  integer :: i, j, b, l, k
 
-!Read the input files
-nconf=800  !Number of steps saved in the dump, take into account the writing frequency
-nbins=210  !Number of bins to divide the cylinder, to calculate separately the pressure on each bin then derive
-jump=1
+  ! per-atom arrays
+  integer,          allocatable :: nnumber(:), nntype(:)
+  real*8,           allocatable :: zz(:)
+  real*8,           allocatable :: pxx_a(:), pyy_a(:), pzz_a(:)
+  real*8,           allocatable :: ptot(:)       ! local pressure anisotropy per atom
 
-!Open the necessary files
-open(9,file='positions.lammpstrj')
+  ! box bounds (updated each frame)
+  real*8 :: lox, hix, loy, hiy, loz, hiz
+  real*8 :: boxx, boxy, boxz, dr
 
-!Read initial data from the dump file, to get natoms
+  ! binned pressure and surface tension
+  real*8, allocatable :: soma(:,:)    ! pressure anisotropy per bin, per frame (nbins, nconf)
+  real*8, allocatable :: temp1(:)     ! time-averaged pressure per bin
+  real*8, allocatable :: sigma(:)     ! γ per frame (mn/m)
+  real*8, allocatable :: dpdr(:)      ! integral of p(z) per frame (bar·å)
 
-nconf=nconf-jump
+  real*8 :: sigmabig                  ! running sum of γ
+  real*8 :: gamma_mean, std, ste      ! final statistics
 
-do i=1,jump
-  print*,'Jumping ',i
-  read(9,*)dummychar
-  read(9,*)dummyint
-  read(9,*)dummychar
-  read(9,*)natoms
-  read(9,*)dummychar
-  read(9,*)lox,hix
-  read(9,*)loy,hiy
-  read(9,*)loz,hiz
-  read(9,*)dummychar
-  do j=1,natoms
-    read(9,*)nnumber,ntype,dummyint,x,y,z,Pxx,Pyy,Pzz !IMPORTANT!! That is the format that the dump file should be: Atom Number, Atom Type, x, y, z, Pxx, Pyy, Pzz, Pxy, Pxz, Pyz
+  integer :: dummyint, natoms
+  real*8  :: xu_dummy, yu_dummy, zu_dummy
+  integer :: ntype_dummy, nmol_dummy
+  integer          :: t0_count, t1_count, count_rate_int, count_max_int
+  real*8           :: wall_start, wall_end
+
+  ! ---------------------------------------------------------------------------
+  ! timing start
+  ! ---------------------------------------------------------------------------
+  call system_clock(t0_count, count_rate_int, count_max_int)
+  wall_start = dble(t0_count) / dble(count_rate_int)
+
+  ! ---------------------------------------------------------------------------
+  ! open dump file and skip 'jump' equilibration frames
+  ! IF PRODUCTION ONLY, set jump = 0
+  ! ---------------------------------------------------------------------------
+  open(9, file=inputtraj, status='old')
+
+  do i = 1, jump
+    write(*,'(a,i6)') 'skipping frame ', i
+
+    read(9,*) dummyint   ! item: timestep (read header as int to be safe)
+    read(9,*) dummyint   ! timestep value
+    read(9,*) dummyint   ! item: number of atoms
+    read(9,*) natoms
+    read(9,*) dummyint   ! item: box bounds
+    read(9,*) lox, hix
+    read(9,*) loy, hiy
+    read(9,*) loz, hiz
+    read(9,*) dummyint   ! item: atoms
+
+    ! allocate temporary arrays on first skip frame, reuse for subsequent ones
+    if (i == 1) then
+      allocate(pxx_a(natoms), pyy_a(natoms), pzz_a(natoms))
+    endif
+
+    do j = 1, natoms
+      read(9,*) dummyint, ntype_dummy, nmol_dummy, &
+                xu_dummy, yu_dummy, zu_dummy, &
+                pxx_a(j), pyy_a(j), pzz_a(j)
+    enddo
   enddo
-enddo
 
-!Calculataing the thickness of each bin
-dr=(hiz-loz)/nbins
-print*,'bin size = ',dr
+  ! free skip arrays before main allocation (if jump > 0)
+  if (jump > 0) deallocate(pxx_a, pyy_a, pzz_a)
 
-!Allocating the variables now that the natoms is known
-allocate(xx(1:natoms),yy(1:natoms),zz(1:natoms),Ptot(1:natoms),soma(1:natoms,1:nconf),temp1(1:nbins))
-allocate(Pxx(1:natoms),Pyy(1:natoms),Pzz(1:natoms),Pxy(1:natoms),Pxz(1:natoms),Pyz(1:natoms))
-allocate(pwater(1:natoms),Ptot1(1:natoms),Ptot2(1:natoms),sigma(1:nconf),dpdr(1:nconf))
-allocate(Ptot3(1:natoms),Ptot4(1:natoms),psili(1:natoms),nntype(1:natoms),idd(1:natoms))
+  ! ---------------------------------------------------------------------------
+  ! read first production frame: get natoms and box for bin setup
+  ! ---------------------------------------------------------------------------
+  read(9,*) dummyint   ! item: timestep
+  read(9,*) dummyint   ! timestep value
+  read(9,*) dummyint   ! item: number of atoms
+  read(9,*) natoms
+  read(9,*) dummyint   ! item: box bounds
+  read(9,*) lox, hix
+  read(9,*) loy, hiy
+  read(9,*) loz, hiz
+  read(9,*) dummyint   ! item: atoms
 
-!Reseting the counters
-sigmabig=0.0
-o=0
-m=0
+  boxx = hix - lox
+  boxy = hiy - loy
+  boxz = hiz - loz
+  dr   = boxz / nbins
 
-do b=1,nconf !loop on step number
-   m=m+1 ! Incrementing counter m, this counter is used if you want to skip some steps (oversized dump file)
-         !to change the number of skipped steps change the ifs in lines 82,99,120,147 from 1 to the desired value.
-         ! You can also set the m at line 65 to a negative value, if you want to skip some initial equilibration steps.
-!Reading dump head
-   read(9,*)dummychar
-   read(9,*)dummyint
-   read(9,*)dummychar
-   read(9,*)dummyint
-   read(9,*)dummychar
-   read(9,*)lox,hix
-   read(9,*)loy,hiy
-   read(9,*)loz,hiz
-   read(9,*)dummychar
+  write(*,'(a,f10.4,a)') ' z-bin width: ', dr, ' å'
 
-   do j=1,natoms !Loop over atom number
-     read(9,*)nnumber,ntype,dummyint,xx(nnumber),yy(nnumber),zz(nnumber),Pxx(nnumber),Pyy(nnumber),Pzz(nnumber) !Reading individual atom data
-     zz(nnumber)=zz(nnumber)-loz
-     idd(j)=nnumber !Setting the data from the temp variables
-     nntype(nnumber)=ntype !Setting the data from the temp variables
-     Ptot(nnumber)=(Pzz(nnumber)-((Pxx(nnumber)+Pyy(nnumber))/2))!/(3*(hix-lox)*(hiy-loy)*(hiz-loz))
-   enddo
+  ! ---------------------------------------------------------------------------
+  ! allocate all working arrays
+  ! ---------------------------------------------------------------------------
+  allocate(nnumber(natoms), nntype(natoms))
+  allocate(zz(natoms))
+  allocate(pxx_a(natoms), pyy_a(natoms), pzz_a(natoms))
+  allocate(ptot(natoms))
+  allocate(soma(nbins, nconf), temp1(nbins))
+  allocate(sigma(nconf), dpdr(nconf))
 
-   do l=1,nbins
-     soma(l,b)=0.0 !Reseting the pressure counter 
-     low=(l-1)*dr !Defining the bin start
-     high=l*dr    !Defining the bin end
-     do k=1,natoms
-       d=zz(k)
-       if ((d.gt.low).and.(d.le.high)) then !testing if it is inside the bin
-         soma(l,b)=soma(l,b)+Ptot(k) !-(Pzz(k)/dr)+(((Pxx(k)+Pyy(k))/2)/(2*(hix-lox)*(hiy-loy))) !Ptot(k) !Accumulate on pressure counter
-       endif
-     enddo
-     soma(l,b)=-soma(l,b)/(3*(hix-lox)*(hiy-loy)*dr)
-   enddo
+  ! ---------------------------------------------------------------------------
+  ! read first production frame atom data
+  ! ---------------------------------------------------------------------------
+  do j = 1, natoms
+    read(9,*) nnumber(j), nntype(j), nmol_dummy, &
+              xu_dummy, yu_dummy, zz(j), &
+              pxx_a(j), pyy_a(j), pzz_a(j)
+    ! shift z so that loz maps to 0 for bin indexing
+    zz(j)   = zz(j) - loz
+    ! local pressure anisotropy integrand: pzz - (pxx + pyy)/2
+    ! units: bar·å³
+    ptot(j) = pzz_a(j) - 0.5d0 * (pxx_a(j) + pyy_a(j))
+  enddo
 
-   !Reseting Counters 
-   temp=0.0
-   temp1=0.0
-   temp2=0.0
-   sigma(b)=0.0
-   dpdr(b)=0
+  ! bin first frame
+  do l = 1, nbins
+    soma(l,1) = 0.0d0
+    do k = 1, natoms
+      ! lower bound inclusive on bin 1; upper bound inclusive on last bin
+      if (zz(k) >= (l-1)*dr .and. zz(k) < l*dr) then
+        soma(l,1) = soma(l,1) + ptot(k)
+      endif
+    enddo
+    ! divide by bin volume to get bar; flip sign (lammps compressive convention)
+    soma(l,1) = -soma(l,1) / (boxx * boxy * dr)
+  enddo
 
- if(m==1)then
-   do i = 2,nbins !Loop over selected bins, those who has derivatives
-     !dV=(hiz-loz)*(hiy-loy)*dr !Volume of the bin
-     !soma(i,b)=soma(i,b)/(3*(hix-lox)*(hiy-loy)*dr)
-     dpdr(b)=dpdr(b)+(dr*(soma(i,b)+soma(i-1,b))/2) 
-   enddo
- endif
+  ! integrate with trapezoidal rule
+  dpdr(1) = 0.0d0
+  do i = 2, nbins
+    dpdr(1) = dpdr(1) + dr * (soma(i,1) + soma(i-1,1)) * 0.5d0
+  enddo
 
- if(m==1)then
-   sigma(b)=(dpdr(b))*1.01325E-5*0.5
-   sigmabig=sigmabig+(sigma(b)) !Summing up all the integral values
-   o=o+1 !incrementing the counter, to get the average after
-   m=0
- endif
- 
- bla=(sigmabig/o) !Calculate the average interfacial tension up to this step
- print *,b,sigma(b),bla !print the step number, the interfacial tension of this step, the average interfacial tension up to this step
-enddo
+  ! convert to mn/m:
+  !   bar·å × 1e5 (pa/bar) × 1e-10 (m/å) × 1e3 (mn/m) × 0.5 (two interfaces)
+  !   = bar·å × 1e-2
+  sigma(1)  = dpdr(1) * 0.5d0 * 1.0d-2
+  sigmabig  = sigma(1)
 
-sigmabig=(sigmabig/o) !calculate the final average for the interfacial tension
-std=0 !reset counter
+  write(*,'(a6,2a16)') 'frame', 'gamma(mn/m)', 'gamma_avg(mn/m)'
+  write(*,'(i6,2f16.5)') 1, sigma(1), sigmabig
 
-do i=1,nconf !Loop over all steps
-  std=std+(sigma(i)-sigmabig)**2 !Calculating the standard deviation
-enddo 
+  ! ---------------------------------------------------------------------------
+  ! main loop: remaining production frames
+  ! ---------------------------------------------------------------------------
+  do b = 2, nconf
 
-std=sqrt(std/DBLE(nconf)) !Calculating the standard deviation
-ste=std/(sqrt(DBLE(nconf))) !Calculating the standard deviation
+    read(9,*) dummyint   ! item: timestep
+    read(9,*) dummyint   ! timestep value
+    read(9,*) dummyint   ! item: number of atoms
+    read(9,*) dummyint   ! natoms (same system)
+    read(9,*) dummyint   ! item: box bounds
+    read(9,*) lox, hix
+    read(9,*) loy, hiy
+    read(9,*) loz, hiz
+    read(9,*) dummyint   ! item: atoms
 
-delta=secnds(t1) !Getting the walltime counter
+    boxx = hix - lox
+    boxy = hiy - loy
+    boxz = hiz - loz
 
-temp=0
-temp1=0
+    do j = 1, natoms
+      read(9,*) nnumber(j), nntype(j), nmol_dummy, &
+                xu_dummy, yu_dummy, zz(j), &
+                pxx_a(j), pyy_a(j), pzz_a(j)
+      zz(j)   = zz(j) - loz
+      ptot(j) = pzz_a(j) - 0.5d0 * (pxx_a(j) + pyy_a(j))
+    enddo
 
-! do i=1,nconf
-!  temp=temp+dpdr(i)
-! enddo
+    ! bin pressure anisotropy
+    do l = 1, nbins
+      soma(l,b) = 0.0d0
+      do k = 1, natoms
+        if (zz(k) >= (l-1)*dr .and. zz(k) < l*dr) then
+          soma(l,b) = soma(l,b) + ptot(k)
+        endif
+      enddo
+      soma(l,b) = -soma(l,b) / (boxx * boxy * dr)
+    enddo
 
-open(47,file='pressures-james.dat')
+    ! trapezoidal integration
+    dpdr(b) = 0.0d0
+    do i = 2, nbins
+      dpdr(b) = dpdr(b) + dr * (soma(i,b) + soma(i-1,b)) * 0.5d0
+    enddo
 
-do i=1,nbins
- do j=1,nconf
-  temp1(i)=temp1(i)+soma(i,j)
- enddo
- temp1(i)=temp1(i)/nconf
- write(47,*)(i-1)*dr,temp1(i)
-enddo
- 
-temp=temp/nconf
+    sigma(b)  = dpdr(b) * 0.5d0 * 1.0d-2
+    sigmabig  = sigmabig + sigma(b)
 
-!Write the final values
-write(*,*)'Execution Time = ',delta
-write(*,'(3A15)')'Surf. Tension','Std. Dev.','Std. Err.'
-write(*,'(3F15.5)')sigmabig,std,ste
-! print*,temp
+    write(*,'(i6,2f16.5)') b, sigma(b), sigmabig / dble(b)
 
-! close(11)
-! close(10)
-close(9)
+  enddo   ! b (frame loop)
 
+  close(9)
 
- END PROGRAM stension_final
+  ! ---------------------------------------------------------------------------
+  ! final statistics
+  ! ---------------------------------------------------------------------------
+  gamma_mean = sigmabig / dble(nconf)
+
+  std = 0.0d0
+  do i = 1, nconf
+    std = std + (sigma(i) - gamma_mean)**2
+  enddo
+  std = sqrt(std / dble(nconf - 1))   ! sample standard deviation
+  ste = std / sqrt(dble(nconf))
+
+  call system_clock(t1_count)
+  wall_end = dble(t1_count) / dble(count_rate_int)
+
+  write(*,'(a,f10.3,a)') ' wall time: ', wall_end - wall_start, ' s'
+  write(*,*)
+  write(*,'(3a16)') 'gamma(mn/m)', 'std_dev', 'std_err'
+  write(*,'(3f16.5)') gamma_mean, std, ste
+
+  ! ---------------------------------------------------------------------------
+  ! write time-averaged pressure profile p(z) to file
+  ! ---------------------------------------------------------------------------
+  open(47, file=outputfile, status='replace')
+  write(47,'(a)') '# z(a)    p(z)(bar)'
+
+  temp1 = 0.0d0
+  do l = 1, nbins
+    do b = 1, nconf
+      temp1(l) = temp1(l) + soma(l,b)
+    enddo
+    temp1(l) = temp1(l) / dble(nconf)
+    write(47,'(2f14.6)') (l-1)*dr + 0.5d0*dr, temp1(l)   ! bin centre
+  enddo
+
+  close(47)
+
+  ! ---------------------------------------------------------------------------
+  ! cleanup
+  ! ---------------------------------------------------------------------------
+  deallocate(nnumber, nntype, zz, pxx_a, pyy_a, pzz_a, ptot)
+  deallocate(soma, temp1, sigma, dpdr)
+
+end program stension_final
